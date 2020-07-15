9d5369e97caee114ba86a00bf2831fb6d6329b3d
Nomaan Ahgharian <code@nomaan.me>
2020-07-14 22:41:21 -0500

    now?

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -22,14 +22,14 @@ export class EmailForm extends Component {
       <>
       Get notified when we launch!
 
-      <form id="signup-form" data-netlify-honeypot="bot-field" name="how-to-say-name" data-netlify="true" onSubmit={this.onSubmit} method="post"  >
+      <form data-netlify-honeypot="bot-field" data-netlify-recaptcha="true" name="how-to-say-name" data-netlify="true" onSubmit={this.onSubmit} method="post"  >
+        <input type="hidden" name="form-name" value="how-to-say-name" />
         <input
           type="email"
           name="email"
           id="email"
           placeholder="Email Address"
         />
-        <input type="hidden" name="form-name" value="how-to-say-name" />
         <input type="submit" value="Sign Up" />
         <span className={`${message ? 'visible success' : ''} message`}>
           {message}
